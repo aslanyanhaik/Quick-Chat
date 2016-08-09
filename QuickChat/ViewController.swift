66d368153ad3ebde2f3c3b830d69ef36840c346c
Haik Aslanyan <haik@mexonis.comgit config --global user.name aslanyanhaik>
2016-08-09 14:45:46 +0400

    Initial Commit

---
+++ QuickChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickChat
+//
+//  Created by Haik Aslanyan on 8/9/16.
+//  Copyright © 2016 Mexonis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
