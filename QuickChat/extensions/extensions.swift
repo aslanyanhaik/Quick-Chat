60cb488b8ad4ab387b652da504e2dc7a68f22efa
Haik Aslanyan <haik@mexonis.comgit config --global user.name aslanyanhaik>
2016-09-12 11:15:01 +0400

    added extensions

--- QuickChat/extensions/extensions.swift
+++ QuickChat/extensions/extensions.swift
@@ -13,5 +13,6 @@ extension UIColor{
     class func rbg(r: CGFloat, g: CGFloat, b: CGFloat) -> UIColor {
         let color = UIColor.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
         return color
+        
     }
 }
