6386f3585881acf53102eea7977fe545160c805a
Haik Aslanyan <aslanyanhaik@gmail.com>
2019-04-22 11:58:34 +0400

    Added login and register functionality to AuthController

--- QuickChat/Presenter/Auth/AuthViewController.swift
+++ QuickChat/Presenter/Auth/AuthViewController.swift
@@ -24,229 +24,124 @@ import UIKit
 
 class AuthViewController: UIViewController {
   
-  
-}
-/*
+  //MARK: IBOutlets
+  @IBOutlet weak var registerImageView: UIImageView!
+  @IBOutlet weak var registerNameTextField: UITextField!
+  @IBOutlet weak var registerEmailTextField: UITextField!
+  @IBOutlet weak var registerPasswordTextField: UITextField!
+  @IBOutlet weak var loginEmailTextField: UITextField!
+  @IBOutlet weak var loginPasswordTextField: UITextField!
+  @IBOutlet weak var cloudsImageView: UIImageView!
+  @IBOutlet weak var cloudsImageViewLeadingConstraint: NSLayoutConstraint!
+  @IBOutlet weak var loginViewTopConstraint: NSLayoutConstraint!
+  @IBOutlet weak var registerViewTopConstraint: NSLayoutConstraint!
 
-class WelcomeVC: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+  //MARK: Private properties
+  private var selectedImage: UIImage?
+  private let manager = UserManager()
+  private let imageService = ImagePickerService()
   
-  //MARK: Properties
-  @IBOutlet weak var darkView: UIView!
-  @IBOutlet weak var spinner: UIActivityIndicatorView!
-  @IBOutlet var registerView: UIView!
-  @IBOutlet var loginView: UIView!
-  @IBOutlet weak var profilePicView: RoundedImageView!
-  @IBOutlet weak var registerNameField: UITextField!
-  @IBOutlet weak var registerEmailField: UITextField!
-  @IBOutlet weak var registerPasswordField: UITextField!
-  @IBOutlet var waringLabels: [UILabel]!
-  @IBOutlet weak var loginEmailField: UITextField!
-  @IBOutlet weak var loginPasswordField: UITextField!
-  @IBOutlet weak var cloudsView: UIImageView!
-  @IBOutlet weak var cloudsViewLeading: NSLayoutConstraint!
-  @IBOutlet var inputFields: [UITextField]!
-  var loginViewTopConstraint: NSLayoutConstraint!
-  var registerTopConstraint: NSLayoutConstraint!
-  let imagePicker = UIImagePickerController()
-  var isLoginViewVisible = true
-  override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-    get {
-      return UIInterfaceOrientationMask.portrait
-    }
+  //MARK: Lifecycle
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    animateClouds()
   }
-  
-  //MARK: Methods
-  func customization()  {
-    self.darkView.alpha = 0
-    self.imagePicker.delegate = self
-    self.profilePicView.layer.borderColor = GlobalVariables.blue.cgColor
-    self.profilePicView.layer.borderWidth = 2
-    //LoginView customization
-    self.view.insertSubview(self.loginView, belowSubview: self.cloudsView)
-    self.loginView.translatesAutoresizingMaskIntoConstraints = false
-    self.loginView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-    self.loginViewTopConstraint = NSLayoutConstraint.init(item: self.loginView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 60)
-    self.loginViewTopConstraint.isActive = true
-    self.loginView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.45).isActive = true
-    self.loginView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
-    self.loginView.layer.cornerRadius = 8
-    //RegisterView Customization
-    self.view.insertSubview(self.registerView, belowSubview: self.cloudsView)
-    self.registerView.translatesAutoresizingMaskIntoConstraints = false
-    self.registerView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-    self.registerTopConstraint = NSLayoutConstraint.init(item: self.registerView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 1000)
-    self.registerTopConstraint.isActive = true
-    self.registerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.6).isActive = true
-    self.registerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
-    self.registerView.layer.cornerRadius = 8
-  }
-  
-  func cloundsAnimation() {
-    let distance = self.view.bounds.width - self.cloudsView.bounds.width
-    self.cloudsViewLeading.constant = distance
-    UIView.animate(withDuration: 15, delay: 0, options: [.repeat, .curveLinear], animations: {
-      self.view.layoutIfNeeded()
-    })
-  }
-  
-  func showLoading(state: Bool)  {
-    if state {
-      self.darkView.isHidden = false
-      self.spinner.startAnimating()
-      UIView.animate(withDuration: 0.3, animations: {
-        self.darkView.alpha = 0.5
-      })
-    } else {
-      UIView.animate(withDuration: 0.3, animations: {
-        self.darkView.alpha = 0
-      }, completion: { _ in
-        self.spinner.stopAnimating()
-        self.darkView.isHidden = true
-      })
-    }
-  }
-  
-  func pushTomainView() {
-    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Navigation") as! NavVC
-    self.show(vc, sender: nil)
-  }
-  
-  func openPhotoPickerWith(source: PhotoSource) {
-    switch source {
-    case .camera:
-      let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
-      if (status == .authorized || status == .notDetermined) {
-        self.imagePicker.sourceType = .camera
-        self.imagePicker.allowsEditing = true
-        self.present(self.imagePicker, animated: true, completion: nil)
-      }
-    case .library:
-      let status = PHPhotoLibrary.authorizationStatus()
-      if (status == .authorized || status == .notDetermined) {
-        self.imagePicker.sourceType = .savedPhotosAlbum
-        self.imagePicker.allowsEditing = true
-        self.present(self.imagePicker, animated: true, completion: nil)
-      }
-    }
-  }
-  
-  @IBAction func switchViews(_ sender: UIButton) {
-    if self.isLoginViewVisible {
-      self.isLoginViewVisible = false
-      sender.setTitle("Login", for: .normal)
-      self.loginViewTopConstraint.constant = 1000
-      self.registerTopConstraint.constant = 60
-    } else {
-      self.isLoginViewVisible = true
-      sender.setTitle("Create New Account", for: .normal)
-      self.loginViewTopConstraint.constant = 60
-      self.registerTopConstraint.constant = 1000
-    }
-    UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
-      self.view.layoutIfNeeded()
-    })
-    for item in self.waringLabels {
-      item.isHidden = true
-    }
-  }
+}
+
+//MARK: IBActions
+extension AuthViewController {
   
   @IBAction func register(_ sender: Any) {
-    for item in self.inputFields {
-      item.resignFirstResponder()
+    guard let name = registerNameTextField.text, let email = registerEmailTextField.text, let password = registerPasswordTextField.text else {
+      return
     }
-    self.showLoading(state: true)
-    User.registerUser(withName: self.registerNameField.text!, email: self.registerEmailField.text!, password: self.registerPasswordField.text!, profilePic: self.profilePicView.image!) { [weak weakSelf = self] (status) in
-      DispatchQueue.main.async {
-        weakSelf?.showLoading(state: false)
-        for item in self.inputFields {
-          item.text = ""
-        }
-        if status == true {
-          weakSelf?.pushTomainView()
-          weakSelf?.profilePicView.image = UIImage.init(named: "profile pic")
-        } else {
-          for item in (weakSelf?.waringLabels)! {
-            item.isHidden = false
-          }
-        }
+    guard !name.isEmpty else {
+      registerNameTextField.borderColor = .red
+      return
+    }
+    guard email.isValidEmail() else {
+      registerEmailTextField.borderColor = .red
+      return
+    }
+    guard password.count > 5 else {
+      registerPasswordTextField.borderColor = .red
+      return
+    }
+    view.endEditing(true)
+    let user = ObjectUser()
+    user.name = name
+    user.email = email
+    user.password = password
+    user.profilePic = selectedImage
+    ThemeService.showLoading(true)
+    manager.register(user: user) {[weak self] response in
+      ThemeService.showLoading(false)
+      switch response {
+        case .failure: self?.showAlert()
+        case .success: self?.dismiss(animated: true, completion: nil)
       }
     }
   }
   
   @IBAction func login(_ sender: Any) {
-    for item in self.inputFields {
-      item.resignFirstResponder()
+    guard let email = loginEmailTextField.text, let password = loginPasswordTextField.text else {
+      return
     }
-    self.showLoading(state: true)
-    User.loginUser(withEmail: self.loginEmailField.text!, password: self.loginPasswordField.text!) { [weak weakSelf = self](status) in
-      DispatchQueue.main.async {
-        weakSelf?.showLoading(state: false)
-        for item in self.inputFields {
-          item.text = ""
-        }
-        if status == true {
-          weakSelf?.pushTomainView()
-        } else {
-          for item in (weakSelf?.waringLabels)! {
-            item.isHidden = false
-          }
-        }
-        weakSelf = nil
+    guard email.isValidEmail() else {
+      loginEmailTextField.borderColor = .red
+      return
+    }
+    guard password.count > 5 else {
+      loginPasswordTextField.borderColor = .red
+      return
+    }
+    view.endEditing(true)
+    let user = ObjectUser()
+    user.email = email
+    user.password = password
+    ThemeService.showLoading(true)
+    manager.login(user: user) {[weak self] response in
+      ThemeService.showLoading(false)
+      switch response {
+      case .failure: self?.showAlert()
+      case .success: self?.dismiss(animated: true, completion: nil)
       }
     }
   }
   
-  @IBAction func selectPic(_ sender: Any) {
-    let sheet = UIAlertController(title: nil, message: "Select the source", preferredStyle: .actionSheet)
-    let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: {
-      (alert: UIAlertAction!) -> Void in
-      self.openPhotoPickerWith(source: .camera)
+  @IBAction func switchViews(_ sender: UIButton) {
+    UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
+      self.view.layoutIfNeeded()
     })
-    let photoAction = UIAlertAction(title: "Gallery", style: .default, handler: {
-      (alert: UIAlertAction!) -> Void in
-      self.openPhotoPickerWith(source: .library)
-    })
-    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-    sheet.addAction(cameraAction)
-    sheet.addAction(photoAction)
-    sheet.addAction(cancelAction)
-    self.present(sheet, animated: true, completion: nil)
   }
   
-  //MARK: Delegates
-  func textFieldDidBeginEditing(_ textField: UITextField) {
-    for item in self.waringLabels {
-      item.isHidden = true
+  @IBAction func profileImage(_ sender: Any) {
+    imageService.pickImage(from: self) {[weak self] image in
+      self?.registerImageView.image = image
+      self?.selectedImage = image
     }
   }
+}
+
+//MARK: Private methods
+extension AuthViewController {
   
+  private func animateClouds() {
+    let distance = view.bounds.width - cloudsImageView.bounds.width
+    self.cloudsImageViewLeadingConstraint.constant = distance
+    UIView.animate(withDuration: 15, delay: 0, options: [.repeat, .curveLinear], animations: {
+      self.view.layoutIfNeeded()
+    })
+  }
+}
+
+//MARK: UITextField Delegate
+extension AuthViewController: UITextFieldDelegate {
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    textField.resignFirstResponder()
-    return true
+    return textField.resignFirstResponder()
   }
   
-  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-    if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
-      self.profilePicView.image = pickedImage
-    }
-    picker.dismiss(animated: true, completion: nil)
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    textField.borderColor = ThemeService.purpleColor
   }
-  
-  //MARK: Viewcontroller lifecycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    self.customization()
-  }
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    self.cloundsAnimation()
-  }
-  
-  override func viewDidDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
-    self.cloudsViewLeading.constant = 0
-    self.cloudsView.layer.removeAllAnimations()
-    self.view.layoutIfNeeded()
-  }
-} */
+}
